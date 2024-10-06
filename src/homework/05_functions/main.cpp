21da0136f264a1cbf73ad95ceb9d5af34fb3d229
RenZhen858 <zen.rhen@g.austincc.edu>
2024-10-06 04:47:07 +0000

    Zhen Ren homework 5

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,37 @@
+#include<iostream>
+#include"func.h"
 
 int main() 
 {
+    int choice;
+	std::string dna;
 
+	while(true)
+	{
+		std::cout<<"Menu\n";
+		std::cout<<"1-Get FC Content\n";
+		std::cout<<"2-Get DNA Complement\n";
+		std::cout<<"3-Exit\n";
+		std::cin>>choice;
+
+		switch(choice)
+		{
+			case 1:
+			    std::cout<<"Enter DNA sequence: ";
+				std::cin>>dna;
+				std::cout<<"GC Content: "<< get_gc_content(dna)<<std::endl;
+				break;
+			case 2:
+			    std::cout<<"Enter DNA sequence: ";
+				std::cin>>dna;
+				std::cout<<"DNA Complement: "<< get_dna_complement(dna)<<std::endl;
+				break;
+			case 3:
+			    return 0;
+			default:
+			    std::cout<<"Invalid choice\n";
+
+		}
+	}
 	return 0;
 }
--- test/homework/05_functions/05_functions_tests.cpp
+++ test/homework/05_functions/05_functions_tests.cpp
@@ -1,7 +1,24 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-
+#include "func.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify get_gc_content")
+{
+	REQUIRE(0.375 == get_gc_content(AGCTATAG));
+	REQUIRE(0.50 == get_gc_content(CGCTATAG));
+}
+
+TEST_CASE("Verify get_reverse_string")
+{
+	REQUIRE(GATATCGA == get_reverse_string(AGCTATAG));
+	REQUIRE(GATATCGC == get_reverse_string(CGCTATAG));
+}
+
+TEST_CASE("Verify get_dna_complement")
+{
+	REQUIRE(ACCGGGTTTT == get_dna_complement(AAAACCCGGT));
+	REQUIRE(ATTTTCCGGG == get_dna_complement(CCCGGAAAAT));
+}
